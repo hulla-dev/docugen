8845205586a663a85989bfad34e591da845a6519
samuelhulla <oficialny.hulla@gmail.com>
2023-09-23 19:29:22 +0200

    ✅ feat: cli

--- cli/cli.ts
+++ cli/cli.ts
@@ -1,18 +1,17 @@
-import { atom } from "nanostores";
-import { type Config, type UserConfig, config as defaultConfig } from "./config";
-import { parseArgs } from "./args";
-import { createDocs, listFiles } from "~src/filesystem";
-import { parseFiles, getDocsData } from "~src/parser";
-import { toMarkdown } from "~src/formatter";
+import { atom } from "nanostores"
+import { createDocs, listFiles } from "~src/filesystem"
+import { toMarkdown } from "~src/formatter"
+import { getDocsData, parseFiles } from "~src/parser"
+import { parseArgs } from "./args"
+import { type Config, type UserConfig, config as defaultConfig } from "./config"
 
 export async function cli(args: string[]) {
   // 0. Initialize config (defaults)
-  const config = atom(defaultConfig);
-  const updateConfig = (newConfig: Partial<Config>) =>
-    config.set({ ...config.get(), ...(newConfig as Config) });
+  const config = atom(defaultConfig)
+  const updateConfig = (newConfig: Partial<Config>) => config.set({ ...config.get(), ...(newConfig as Config) })
   // 1. Handle CLI arguments (update config)
-  await parseArgs(args, updateConfig);
-  const userConfig = config.get() as UserConfig;
+  await parseArgs(args, updateConfig)
+  const userConfig = config.get() as UserConfig
   // 2. List files to parse
   const relevantFiles = listFiles(userConfig.includes, userConfig.files, userConfig.excludes)
   // 3. Parse files, create a lexical tree and create documentation data
