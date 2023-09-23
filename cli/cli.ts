8e526745870b474d978ae13033df9aaa4bcdc1ae
samuelhulla <oficialny.hulla@gmail.com>
2023-09-23 18:16:24 +0200

    ✅ feat: cli

---
+++ cli/cli.ts
@@ -0,0 +1,27 @@
+import { atom } from "nanostores";
+import { type Config, type UserConfig, config as defaultConfig } from "./config";
+import { parseArgs } from "./args";
+import { createDocs, listFiles } from "~src/filesystem";
+import { parseFiles, getDocsData } from "~src/parser";
+import { toMarkdown } from "~src/formatter";
+
+export async function cli(args: string[]) {
+  // 0. Initialize config (defaults)
+  const config = atom(defaultConfig);
+  const updateConfig = (newConfig: Partial<Config>) =>
+    config.set({ ...config.get(), ...(newConfig as Config) });
+  // 1. Handle CLI arguments (update config)
+  await parseArgs(args, updateConfig);
+  const userConfig = config.get() as UserConfig;
+  // 2. List files to parse
+  const relevantFiles = listFiles(userConfig.includes, userConfig.files, userConfig.excludes)
+  // 3. Parse files, create a lexical tree and create documentation data
+  const rawLexTree = await parseFiles(relevantFiles)
+  // 4. Prepare data for transformation
+  // These essentialy create { [path]: Array<{ declaration: Declaration, docs: Docs }> } mappings
+  const data = await getDocsData(rawLexTree)
+  // 5. Transform data to markdown
+  const md = toMarkdown(data, userConfig.adapter)
+  // 6. Write to files
+  await createDocs(md, userConfig.outDir)
+}
